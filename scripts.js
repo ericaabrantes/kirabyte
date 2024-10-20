42d58e0034778c8435729880a418ec9828422db4
Érica Abrantes <ericabrantes@hotmail.com>
2024-10-20 08:44:04 -0300

    integracao de js , formulario, carrossel dos projetos

---
+++ scripts.js
@@ -0,0 +1,57 @@
+// carrossel projetos
+let slideIndex = 0;
+mostrarSlides(slideIndex);
+
+function mostrarSlides(n) {
+    let slides = document.querySelectorAll(".slide");
+    if (n >= slides.length) {
+        slideIndex = 0;
+    }
+    if (n < 0) {
+        slideIndex = slides.length - 1;
+    }
+
+    slides.forEach((slide) => {
+        slide.style.display = "none";
+    });
+
+    slides[slideIndex].style.display = "block";
+}
+
+document.querySelector(".next").addEventListener("click", () => {
+    slideIndex++;
+    mostrarSlides(slideIndex);
+});
+
+document.querySelector(".prev").addEventListener("click", () => {
+    slideIndex--;
+    mostrarSlides(slideIndex);
+});
+
+
+
+// formulario de email integracao
+
+document.querySelector('.contato__formulario').addEventListener('submit', function(event) {
+    event.preventDefault(); // Previne o comportamento padrão do formulário
+
+    // Coleta os dados do formulário
+    var params = {
+        nome: document.getElementById('nome').value,
+        whatsapp: document.getElementById('whatsapp').value,
+        email: document.getElementById('email').value,
+        mensagem: document.getElementById('mensagem').value
+    };
+
+    // Envia o e-mail através do EmailJS
+    emailjs.send("service_07b5nb2", "template_9dfrnoo", params)
+    .then(function(response) {
+        // Exibe mensagem de sucesso
+        alert('Formulário enviado com sucesso!');
+        
+        // Limpa os campos do formulário
+        event.target.reset(); 
+    }, function(error) {
+        alert('Ocorreu um erro ao enviar o formulário: ' + JSON.stringify(error));
+    });
+});
