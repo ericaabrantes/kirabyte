1b9fefedb33a5f00ef60fb7741cf7c94511f5e14
Érica Abrantes <ericabrantes@hotmail.com>
2024-10-21 10:15:22 -0300

    melhoria formulario js, envio somente pelo botao

--- scripts.js
+++ scripts.js
@@ -32,7 +32,7 @@ document.querySelector(".prev").addEventListener("click", () => {
 
 // formulario de email integracao
 
-document.querySelector('.contato__formulario').addEventListener('submit', function(event) {
+document.querySelector('.contato__formulario').addEventListener('submit', function (event) {
     event.preventDefault(); // Previne o comportamento padrão do formulário
 
     // Coleta os dados do formulário
@@ -45,13 +45,22 @@ document.querySelector('.contato__formulario').addEventListener('submit', functi
 
     // Envia o e-mail através do EmailJS
     emailjs.send("service_07b5nb2", "template_9dfrnoo", params)
-    .then(function(response) {
-        // Exibe mensagem de sucesso
-        alert('Formulário enviado com sucesso!');
-        
-        // Limpa os campos do formulário
-        event.target.reset(); 
-    }, function(error) {
-        alert('Ocorreu um erro ao enviar o formulário: ' + JSON.stringify(error));
+        .then(function (response) {
+            // Exibe mensagem de sucesso
+            alert('Formulário enviado com sucesso! Entraremos em contato em breve');
+
+            // Limpa os campos do formulário
+            event.target.reset();
+        }, function (error) {
+            alert('Ocorreu um erro ao enviar o formulário: ' + JSON.stringify(error));
+        });
+});
+
+// Prevenir envio ao pressionar "Enter"
+document.querySelectorAll('.contato__campos').forEach(function (input) {
+    input.addEventListener('keydown', function (event) {
+        if (event.key === 'Enter') {
+            event.preventDefault(); // Previne o envio do formulário ao pressionar Enter
+        }
     });
 });
